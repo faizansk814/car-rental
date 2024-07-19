358fbba637a532729aae0edd6fe2952ca992a668
faizansk <faizansk814@gmail.com>
2024-07-19 15:57:46 +0530

    get vehicle details by its type route done

--- car-rental-backend/routes/vehicle.js
+++ car-rental-backend/routes/vehicle.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Vehicle , BookVehicle, Sequelize } = require("../models");
+const { Vehicle, BookVehicle, Sequelize } = require("../models");
 const vehiclerouter = express.Router();
 
 // API to create a new vehicle
@@ -35,33 +35,47 @@ vehiclerouter.post("/vehicle-types", async (req, res) => {
   }
 });
 
-vehiclerouter.post('/book-vehicle', async (req, res) => {
-    try {
-      const { vehicleId, startDate, endDate } = req.body;
-  
-      // Check if the vehicle is already booked
-      const existingBooking = await BookVehicle.findOne({
-        where: {
-          vehicleId,
-          startDate: {
-            [Sequelize.Op.lte]: endDate,
-          },
-          endDate: {
-            [Sequelize.Op.gte]: startDate,
-          },
+vehiclerouter.post("/book-vehicle", async (req, res) => {
+  try {
+    const { vehicleId, startDate, endDate } = req.body;
+
+    // Check if the vehicle is already booked
+    const existingBooking = await BookVehicle.findOne({
+      where: {
+        vehicleId,
+        startDate: {
+          [Sequelize.Op.lte]: endDate,
         },
-      });
-  
-      if (existingBooking) {
-        return res.status(400).json({ message: 'Vehicle is already booked for the selected dates' });
-      }
-  
-      // Create a new booking
-      const booking = await BookVehicle.create({ vehicleId, startDate, endDate });
-      res.status(201).json(booking);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+        endDate: {
+          [Sequelize.Op.gte]: startDate,
+        },
+      },
+    });
+
+    if (existingBooking) {
+      return res
+        .status(400)
+        .json({ message: "Vehicle is already booked for the selected dates" });
     }
-  });
+
+    // Create a new booking
+    const booking = await BookVehicle.create({ vehicleId, startDate, endDate });
+    res.status(201).json(booking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+vehiclerouter.post("/vehicles-by-type", async (req, res) => {
+  try {
+    const { type } = req.body;
+    const vehicles = await Vehicle.findAll({
+      where: { type },
+    });
+    res.status(200).json(vehicles);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 module.exports = vehiclerouter;
